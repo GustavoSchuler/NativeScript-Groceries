5585ac06cad1d3cd5467f25767e6b2334d2b87d0
Gustavo Schüler <luis.schuler@gmail.com>
2016-07-07 17:17:48 -0300

    Adding files

---
+++ app/views/login/login.js
@@ -0,0 +1,29 @@
+var dialogsModule = require("ui/dialogs");
+var frameModule = require("ui/frame");
+var viewModule = require("ui/core/view");
+var UserViewModel = require("../../shared/view-models/user-view-model");
+var user = new UserViewModel();
+
+exports.loaded = function(args) {
+    var page = args.object;
+    page.bindingContext = user;
+};
+
+exports.register = function() {
+	frameModule.topmost().navigate("views/register/register");
+};
+
+exports.signIn = function() {
+	user.login()
+        .catch(function(error) {
+            console.log(error);
+            dialogsModule.alert({
+                message: "Unfortunately we could not find your account.",
+                okButtonText: "OK"
+            });
+            return Promise.reject();
+        })
+        .then(function() {
+            frameModule.topmost().navigate("views/list/list");
+        });
+};
